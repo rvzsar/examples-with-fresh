c642d7179264e72989cb21d57eb5b25996c49637
rvzsar <51318232+rvzsar@users.noreply.github.com>
2025-09-07 15:40:41 +0400

    Update dev.ts

--- dev.ts
+++ dev.ts
@@ -1,11 +1,9 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
-import { Builder } from "fresh/dev";
+import dev from "$fresh/dev.ts";
+import config from "./fresh.config.ts";
 
-const builder = new Builder();
+import { setupDatabase } from "./utils/database.ts";
 
-if (Deno.args.includes("build")) {
-  await builder.build();
-} else {
-  await builder.listen(() => import("./main.ts"));
-}
+await setupDatabase();
+await dev(import.meta.url, "./main.ts", config);
